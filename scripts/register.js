d847b456487157db8814b72e6ea83e100e7dea7b
DjLeChuck <djlechuck@gmail.com>
2022-07-31 16:48:51 +0200

    feat: add an option to disable auto registration of translations

--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,26 @@
 Hooks.on('init', () => {
+  game.settings.register('pf1-fr-babele', 'autoRegisterBabel', {
+    name: 'Activer automatiquement la traduction via Babele',
+    hint: 'Met automatiquement en place les traductions au sein de Babele sans avoir besoin de pointer vers le répertoire contenant les traductions.',
+    scope: 'world',
+    config: true,
+    default: true,
+    type: Boolean,
+    onChange: value => {
+      if (value) {
+        autoRegisterBabel();
+      }
+
+      window.location.reload();
+    },
+  });
+
+  if (game.settings.get('pf1FrBabele', 'autoRegisterBabel')) {
+    autoRegisterBabel();
+  }
+});
+
+function autoRegisterBabel() {
   if (typeof Babele !== 'undefined') {
     Babele.get().register({
       module: 'pf1-fr-babele',
@@ -6,4 +28,4 @@ Hooks.on('init', () => {
       dir: 'compendium/fr',
     });
   }
-});
+}
